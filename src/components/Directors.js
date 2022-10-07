b1a548b26830d57cca2dbb6ca165e20a85e43693
nxoo <nxoo.dev@gmail.com>
2022-10-07 15:29:14 +0300

    actors & directros

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { directors } from "../data";
+import {directors} from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+    return (
+        <div>
+            <h1>Directors Page</h1>
+            <div>
+                {
+                    directors.map((director) => (
+                        <div key={director.name}>
+                            Director: {director.name}
+                            <ul> Movies:
+                                {director.movies.map((movie) => (
+                                    <li key={movie}>{movie}</li>))}
+                            </ul>
+                        </div>)
+                    )}
+            </div>
+        </div>
+    )
 }
 
 export default Directors;
