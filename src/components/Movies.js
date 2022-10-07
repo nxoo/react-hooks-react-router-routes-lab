3952aa8c128372a972ca30bed60ada548d46a1fb
nxoo <nxoo.dev@gmail.com>
2022-10-07 15:28:05 +0300

    movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { movies } from "../data";
+import {movies} from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+    return (
+        <div>
+            <h1>Movies Page</h1>
+            <div>
+                {
+                    movies.map((movie) => (
+                        <div key={movie.title}>
+                            Movie: {movie.title} <br></br>
+
+                            Time: {movie.time}
+                            <ul>
+                                {movie.genres.map((genre) => (
+                                    <li key={genre}>{genre}</li>))}
+                            </ul>
+                        </div>)
+                    )}
+            </div>
+        </div>
+    )
 }
 
 export default Movies;
